a87018604671204d2fd441c7c82dbaf9cbda185b
pravin desai <pravindesai100@gmail.com>
2022-05-25 22:29:55 +0530

    Navigation Component with arguments added

--- app/src/main/java/com/pravin/jetpackcompose/screens/Screen.kt
+++ app/src/main/java/com/pravin/jetpackcompose/screens/Screen.kt
@@ -3,4 +3,13 @@ package com.pravin.jetpackcompose.screens
 sealed class Screen(var routs:String) {
     object FirstScreen:Screen("First Screen")
     object SecondScreen:Screen("Second Screen")
+
+    fun withArgs(vararg args:String):String{
+        return buildString {
+            append(routs)
+            args.forEach {arg->
+                append("/$arg")
+            }
+        }
+    }
 }
